27853afd9134e2c4574c964d6202c5803eb790a8
Salgado, Anahí X.-ND <anahi.x.salgado.-nd@disney.com>
2020-05-20 02:23:42 -0500

    Changing name to Create Account

--- app/src/main/java/com/anncode/androidnavigationcomponent/CreateAccountFragment.kt
+++ app/src/main/java/com/anncode/androidnavigationcomponent/CreateAccountFragment.kt
@@ -9,14 +9,14 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass.
  */
-class RegisterFragment : Fragment() {
+class CreateAccountFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        return inflater.inflate(R.layout.fragment_create_account, container, false)
     }
 
 }
