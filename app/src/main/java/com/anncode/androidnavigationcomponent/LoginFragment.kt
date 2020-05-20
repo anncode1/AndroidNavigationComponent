f985058f4ad66e77ba2acdee32c1f6bcd4ea3f80
Salgado, Anahí X.-ND <anahi.x.salgado.-nd@disney.com>
2020-05-20 03:21:42 -0500

    creating two actions toCreateAccountFragment and toHomeFragment

--- app/src/main/java/com/anncode/androidnavigationcomponent/LoginFragment.kt
+++ app/src/main/java/com/anncode/androidnavigationcomponent/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +19,22 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_login.setOnClickListener {
+            findNavController().navigate(
+                LoginFragmentDirections.toHomeFragment()
+            )
+        }
+
+        btn_register.setOnClickListener {
+            findNavController().navigate(
+                LoginFragmentDirections.toCreateAccountFragment()
+            )
+        }
     }
 
 }
