b049e2698af5da316d21861cc4a413df9ba0607b
Salgado, Anahí X.-ND <anahi.x.salgado.-nd@disney.com>
2020-05-21 17:23:36 -0500

    Handle User Argument

--- app/src/main/java/com/anncode/androidnavigationcomponent/HomeFragment.kt
+++ app/src/main/java/com/anncode/androidnavigationcomponent/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +19,15 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_profile.setOnClickListener {
+            val user = User("Ann", "Teach")
+            val navDirection = HomeFragmentDirections.toMyProfileFragment(user)
+            findNavController().navigate(navDirection)
+        }
     }
 
 }
--- app/src/main/java/com/anncode/androidnavigationcomponent/MyProfileFragment.kt
+++ app/src/main/java/com/anncode/androidnavigationcomponent/MyProfileFragment.kt
@@ -5,11 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class MyProfileFragment : Fragment() {
+
+    private val args: MyProfileFragmentArgs? by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +21,14 @@ class MyProfileFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        args?.user?.let {
+            val profile = "\n\nName: ${it.name}" + "\nHobby: ${it.hobby}"
+            tv_profile.append(profile)
+        }
     }
 
 }
