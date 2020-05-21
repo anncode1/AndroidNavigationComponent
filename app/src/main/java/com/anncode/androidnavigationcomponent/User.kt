b2cb7d83bfaf5622b6ba1fff48bbb91137fc85c6
Salgado, Anahí X.-ND <anahi.x.salgado.-nd@disney.com>
2020-05-21 16:51:24 -0500

    user data class

---
+++ app/src/main/java/com/anncode/androidnavigationcomponent/User.kt
@@ -0,0 +1,8 @@
+package com.anncode.androidnavigationcomponent
+
+import java.io.Serializable
+
+/**
+ * Created by anahi.salgado on 21/05/2020
+ */
+data class User(val name: String, val hobby: String): Serializable
