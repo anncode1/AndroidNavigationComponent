995b18a63f0365709450f1012915f0fd5bd79ee6
Salgado, Anahí X.-ND <anahi.x.salgado.-nd@disney.com>
2020-05-21 12:05:34 -0500

    popExitAnim animations and button styles

--- app/src/main/java/com/anncode/androidnavigationcomponent/CreateAccountFragment.kt
+++ app/src/main/java/com/anncode/androidnavigationcomponent/CreateAccountFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_create_account.*
 
 /**
  * A simple [Fragment] subclass.
@@ -17,6 +19,15 @@ class CreateAccountFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_create_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_create.setOnClickListener {
+            findNavController().navigate(
+                CreateAccountFragmentDirections.toLoginFragment()
+            )
+        }
     }
 
 }
